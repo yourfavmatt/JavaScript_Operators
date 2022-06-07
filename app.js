d4a836ce1f9cff5ac000dc224ba2311041cb927f
matt Klein <mwklein212@gmail.com>
2022-06-07 15:27:02 -0400

    completed exercise

--- app.js
+++ app.js
@@ -3,17 +3,64 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+
 console.log("EXERCISE 1:\n==========\n");
 
-// YOUR CODE HERE
+var a = 20;
+var b = 4;
+let sum = a + b;
+let minus = a - b;
+let times = a * b;
+let division = a / b;
+a + b; 24
+a - b; 20
+a * b; 80
+a / b; 5
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+
 
 // Exercise 2
-console.log("EXERCISE 2:\n==========\n");
 
-// YOUR CODE HERE
+
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+
+console.log(num + str);
+console.log(num + str2);
+console.log(num + isPresent);
+console.log(firstName + num);
+console.log(isPresent + str);
+console.log(firstName + lastName);
+
+
 
 // Exercise 3
-console.log("EXERCISE 3:\n==========\n");
 
-// YOUR CODE HERE
+val = 5; 
+str = "5";
+str2 = "five";
+isPresent = false;
+
+
+console.log(val==str);
+console.log(val===str);
+console.log(!isPresent);
+console.log("eleven" == str2 && val >=str);
+console.log(!isPresent || isPresent);
+console.log(0 == false);
+console.log(0 === false);
+console.log(0 != false);
+console.log(0 !== false);
+
+
+
+
+
